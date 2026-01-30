1c325a164208e87c452386d103a43a46a2dc97a2
Connor Wilson-Groark <connorwg@gmail.com>
2026-01-30 11:54:10 -0500

    Re-configured api testing component

--- apps/web/src/components/TestApi.tsx
+++ apps/web/src/components/TestApi.tsx
@@ -1,16 +1,42 @@
 import { useState } from "react"
+import { useAuth0 } from "@auth0/auth0-react"
 
 const TestApi = () => {
   
     const [result, setResult] = useState<string>("")
+    const { getAccessTokenSilently, isAuthenticated } = useAuth0()
   
     const callApi = async () => {
         try {
-            const res = await fetch("http://localhost:5274/api/v1/");
+            if (!isAuthenticated){
+                setResult("User is not authenticated");
+                return;
+            }
+
+            // Get the access token
+            const token = await getAccessTokenSilently();
+            // Call protected endpoint with Bearer token
+            const res = await fetch("http://localhost:5274/api/v1/secret", {
+                headers: {
+                Authorization: `Bearer ${token}`,
+                },
+            });
+
+            //Handle response
+            if (!res.ok) {
+                const text = await res.text();
+                setResult(`Error ${res.status}: ${text}`);
+                return;
+            }
+
             const text = await res.text();
             setResult(text);
         } catch (err) {
-            setResult("Error fetching data from API");
+            if (err instanceof Error) {
+                setResult(`Error calling API: ${err.message}`);
+            } else {
+                setResult("Unknown error calling API");
+            }
             console.error(err);
         }
     }
